2032993af7703a161e9f7996f4d590910da0b447
canoziia <canoziia@projectk.org>
2025-11-07 23:47:45 +0800

    fix workdir path

--- scripts/build-all.sh
+++ scripts/build-all.sh
@@ -4,8 +4,7 @@ sudo apt update
 sudo apt-get install -y gcc-aarch64-linux-gnu u-boot-tools bc make gcc ccache libc6-dev libncurses5-dev libssl-dev bison flex device-tree-compiler libelf-dev kmod libdw-dev libdebuginfod-dev systemtap-sdt-dev libunwind-dev libslang2-dev libperl-dev python3-dev python3 llvm-dev libzstd-dev libnuma-dev libbabeltrace-ctf-dev libcapstone-dev libpfm4-dev libtraceevent-dev libtracefs-dev default-jdk clang binutils-dev libcap-dev libbpf-dev asciidoc xmlto u-boot-tools
 
 git clone https://github.com/huihuimoe/vyos-arm64-build
-cd $PROJECT_ROOT/vyos-arm64-build
-git clone https://github.com/vyos/vyos-build
+git clone https://github.com/vyos/vyos-build vyos-arm64-build/vyos-build
 
 bash scripts/patch-and-build-vyos-1x.sh
 
