f4ab941647872f12deef46ecd51ad06ed7c7ab9b
canoziia <canoziia@projectk.org>
2025-11-10 22:23:54 +0800

    delete some packages

--- scripts/patch-and-build-kernel-related-packages.sh
+++ scripts/patch-and-build-kernel-related-packages.sh
@@ -9,6 +9,6 @@ patch -p1 < $PROJECT_ROOT/patches/vyos-build/0011-build-linux-package-toml.patch
 patch -p1 < $PROJECT_ROOT/patches/vyos-build/0012-build-jool.patch
 
 cd $VYOS_BUILD_ROOT/scripts/package-build/linux-kernel
-./build.py --packages linux-firmware qat igb ixgbe ixgbevf jool nat-rtsp ovpn-dco # seems that accel-ppp-ng is not required, I have confirmed jool nat-rtsp ovpn-dco must be built
+./build.py --packages linux-firmware jool nat-rtsp ovpn-dco # seems that accel-ppp-ng is not required, I have confirmed jool nat-rtsp ovpn-dco must be built. qat igb ixgbe ixgbevf is important for amd64 but 6.17 will failed
 ls -la ./*.deb
 mv ./*.deb $VYOS_BUILD_ROOT/packages/
