e6ccad228468ee792333f454576cb617ed25ba4d
canoziia <canoziia@projectk.org>
2025-12-04 17:50:07 +0800

    tmpfix kernel version

--- scripts/patch-and-build-kernel.sh
+++ scripts/patch-and-build-kernel.sh
@@ -12,6 +12,7 @@ cp $PROJECT_ROOT/patches/vyos-build/0002-inotify-support-for-stackable-filesyste
 cp $PROJECT_ROOT/patches/vyos-build/0003-build-linux-perf-package.patch $VYOS_BUILD_ROOT/scripts/package-build/linux-kernel/patches/kernel/
 rm -rf $VYOS_BUILD_ROOT/scripts/package-build/linux-kernel/patches/kernel/v4-0001-nft_ct-Added-nfct_seqadj_ext_add-for-DNAT-ed-conn.patch
 patch -p1 < $PROJECT_ROOT/patches/main/linux-kernel-defconfig.patch
+patch -p1 < $PROJECT_ROOT/patches/vyos-build/9997-linux-version-tmp-fix.patch
 
 cd $VYOS_BUILD_ROOT/scripts/package-build/linux-kernel
 ./build.py --packages linux-kernel
